02b3e809fc16afbcc9d915677851c716863b5774
Ken Peltzer <kpeltzer@thrillist.com>
2014-02-26 20:47:01 -0500

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kenpeltzer')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
