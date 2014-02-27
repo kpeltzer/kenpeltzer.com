5c7cd494e1637a0590a37093630893c6e9f89cca
Ken Peltzer <kpeltzer@gmail.com>
2014-02-27 00:15:54 -0500

    More work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,19 @@ angular.module('kenpeltzer', [
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/homepage.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'MainCtrl'
+      })
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'MainCtrl'
+      })
+      .when('/blog', {
+        templateUrl: 'views/blog.html',
         controller: 'MainCtrl'
       })
       .otherwise({
